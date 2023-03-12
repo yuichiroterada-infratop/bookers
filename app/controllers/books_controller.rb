acbb4edc9a05523525802502401057b065e1544e
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-12 12:43:48 +0000

    [Fin]保存機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,43 @@
 class BooksController < ApplicationController
-  def index
-  end
-
-  def create
-  end
 
   def new
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
-  def show
-  end
-
-  def update
-  end
-
   def destroy
   end
 
+  def index
+    @books = Book.all.order(id: :asc)
+    @book = Book.new  #<-ここに書いちゃうことで解決！
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
+    else
+      render :index
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
