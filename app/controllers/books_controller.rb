3dd6378cd0253fb506744ed87aa6299164f770a0
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-12 13:11:16 +0000

    [Fin]destroy機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
   end
 
   def index
