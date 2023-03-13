bf3f658bbb71c2e1d633fc489716a7743f66413b
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-13 06:34:33 +0000

    [Fin]Bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,10 +26,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Your Book was successfully created!"
+      flash[:notice] = "Book has been successfully created!"
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
+      @books = Book.all.order(id: :asc)
       render :index
     end
   end
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Your Book was successfully updated!"
+      flash[:notice] = "Book has been successfully updated!"
       redirect_to book_path(@book.id)
     else
       render :edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   get '/top' => "homes#top"
   resources :books
 end
